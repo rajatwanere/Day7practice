7c6377ddaffce8e9acd5c3772388c00fb9a730cf
rajatwanere <rajatwanere999@gmail.com>
2023-06-05 19:19:04 +0530

    added:stringdemo.java

---
+++ Stringdemo.java
@@ -0,0 +1,16 @@
+package com.demojava;
+
+public class Stringdemo {
+    public static void main(String[] args) {
+         String str1="rajat";
+         String str2="pankaj";
+
+         if(str1.equals(str2)){
+             System.out.println("string is equal");
+         }
+         else{
+             System.out.println("string is not equal");
+         }
+    }
+
+}
